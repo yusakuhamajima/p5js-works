415245a50c89a42443b1736d9346579b192e1f63
yusakuhamajima <yusakuhamajima@gmail.com>
2019-10-01 23:54:50 +0900

    folder up

---
+++ works/js/works-001.js
@@ -0,0 +1,25 @@
+let col;
+let colrect;
+
+function setup() {
+    createCanvas(800, 600, P2D);
+    colorMode(HSB, 360, 100, 100, 100);
+
+    col = color(220, 100, 100, 20);
+
+    background(col);
+    fill(col);
+    strokeWeight(1);
+}
+
+function draw() {
+    colrect = color(map(220, 150, 300, 0, mouseX), 100, 100, 100);
+    stroke(colrect);
+    rect(0, 0, mouseX, mouseY);
+}
+
+function keyPressed() {
+    if (key == 's') {
+        save("p5js-001-04.png");
+    }
+}
