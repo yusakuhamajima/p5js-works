a37355b4a2f998ecef6f977e92ac256f4f0da2bd
yusakuhamajima <yusakuhamajima@gmail.com>
2019-12-08 19:09:06 +0900

    mobile second

---
+++ mobile/js/mo-001.js
@@ -0,0 +1,8 @@
+function setup() {
+    var canvas = createCanvas(windowWidth, windowHeight, P2D);
+    canvas.parent("canvas");
+}
+function draw() {
+    background(0);
+    ellipse(100, 100, 100, 100);
+}
