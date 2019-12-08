016b088067bf86241921b026dc1f323ac2838936
yusakuhamajima <yusakuhamajima@gmail.com>
2019-12-08 19:15:49 +0900

    001

--- mobile/js/mo-001.js
+++ mobile/js/mo-001.js
@@ -1,8 +1,29 @@
+var x, y, z, d;
+var moveellipse;
+
 function setup() {
     var canvas = createCanvas(windowWidth, windowHeight, P2D);
     canvas.parent("canvas");
+    moveellipse = new MoveEllipse();
+
 }
 function draw() {
     background(0);
-    ellipse(100, 100, 100, 100);
+    moveellipse.draw();
+}
+
+class MoveEllipse {
+    constructor() {
+        this.x = 100;
+        this.y = height / 2;
+        this.w = 30;
+        this.d = -1;
+    }
+    draw() {
+        ellipse(this.x, this.y, this.w, this.w);
+        this.x = this.x + 10 * this.d;
+        if (this.x < 0 || this.x > width) {
+            this.d = this.d * -1;
+        }
+    }
 }
