e4e288bffee7db1b82b4b4e9053d2ade94833dea
yusakuhamajima <yusakuhamajima@gmail.com>
2019-12-28 21:09:10 +0900

    説明コメントあり

---
+++ mobile/js/mo-002.js
@@ -0,0 +1,51 @@
+let NUM = 600;
+let moveE = new Array(NUM);
+let loc, vel, col, diameter, sw;
+
+function setup() {
+    var canvas = createCanvas(windowWidth, windowHeight, P2D);
+    canvas.parent("canvas");
+    colorMode(HSB, 360, 100, 100, 100);
+    noStroke();
+    frameRate(60);
+
+    for (let i = 0; i < NUM; i++) {
+        moveE[i] = new Mover();
+    }
+}
+
+function draw() {
+    background(220, 100, 100, 100);
+
+    for (let i = 0; i < NUM; i++) {
+        moveE[i].moveEllipse();
+    }
+}
+
+class Mover {
+    constructor() {
+        this.loc = createVector(random(width), random(height));
+        this.vel = createVector(random(0, 3), random(0, 3));
+        this.col = color(random(360), 100, 100, random(20, 50));
+        this.diameter = random(10, 60);
+        this.sw = random(0.5, 2);
+    }
+
+    moveEllipse() {
+        ellipse(this.loc.x, this.loc.y, this.diameter, this.diameter);
+        this.loc.add(this.vel);
+        fill(this.col);
+        // noFill();
+        stroke(this.col);
+        strokeWeight(this.sw);
+
+        if (this.loc.x < 0 || this.loc.x > width) {
+            // this.vel.x = this.vel.x * -1;
+            this.loc.x = 0;
+        }
+        if (this.loc.y < 0 || this.loc.y > height) {
+            // this.vel.y = this.vel.y * -1;
+            this.loc.y = 0;
+        }
+    }
+}
