4d975311cf35b95ad752f0979722ade9a29eaf48
yusakuhamajima <yusakuhamajima@gmail.com>
2020-01-09 21:25:58 +0900

    tokei

--- mobile/js/mo-003.js
+++ mobile/js/mo-003.js
@@ -1,14 +1,42 @@
+var x, y, w, h;
+var onesecond;
+var cnt;
+var sc;
+
 function setup() {
-    var canvas = createCanvas(windowWidth, windowHeight, P2D);
-    canvas.parent('canvas');
+    createCanvas(windowWidth, windowHeight, P2D);
+    x = 0;
+    y = 0;
+    w = 400;
+    h = 400;
+    onesecond = TAU / 60;
+    console.log(onesecond);
+    console.log(cnt);
+    angle = -HALF_PI;
 }
+
 function draw() {
     background(0);
-    rect(100, 100, 100, 100);
+
+    // time num
+    noStroke();
+    fill(255);
+    textAlign(CENTER, CENTER);
+    textSize(100);
+    sc = second();
+    cnt = onesecond * sc;
+    text(sc, width / 2, height / 2);
+
+    translate(width / 2, height / 2);
+    rotate(angle);
+    noFill();
+    stroke(255);
+    strokeWeight(10);
+    arc(x, y, w, h, 0, cnt);
 }
-function keyPressed() {
+
+function keyTyped() {
     if (key === 's') {
-        console.log('key-s');
-        saveCanvas('mo-003_01', 'png');
+        saveCanvas('mo-003', 'png');
     }
 }
